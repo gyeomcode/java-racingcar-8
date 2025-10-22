56f5a20db6a703d0041abc351e5f4c961c5db9b1
gyeomcode <gyeomcode@gmail.com>
2025-10-23 01:01:05 +0900

    feat: 허용되지 않은 자동차 이름을 입력하면 예외가 발생한다
    - 자동차 이름은 빈 문자열을 허용하지 않는다.
    - 자동차 이름은 5자 이하만 허용한다.
    - 자동차 리스트의 길이가 1 이상만 허용한다.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class InputView {
     private static final String CAR_NAME_SEPARATOR = ",";
-    
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
@@ -17,6 +18,15 @@ public class InputView {
     public List<String> parseCarNames(String input) {
         List<String> carNames = Arrays.stream(input.split(CAR_NAME_SEPARATOR)).map(String::trim).toList();
 
+        validateCarNames(carNames);
+
         return carNames;
+    }
+
+    private void validateCarNames(List<String> carNames) {
+        if (carNames.isEmpty() || carNames.stream()
+                .anyMatch(carName -> carName.length() > CAR_NAME_MAX_LENGTH || carName.isEmpty())) {
+            throw new IllegalArgumentException("자동차 이름이 올바르지 않습니다.");
+        }
     }
 }
